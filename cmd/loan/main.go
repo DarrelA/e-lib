d3d0c6a992dd59053a6b988121bf3100f4d6544a
DarrelA <darrelaiscoding@gmail.com>
2025-03-06 15:42:58 +0800

    `books.json`: Add dummy data

--- cmd/loan/main.go
+++ cmd/loan/main.go
@@ -1,5 +1,41 @@
 package main
 
-func main() {
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"time"
 
+	"github.com/DarrelA/e-lib/internal/apperrors"
+	"github.com/DarrelA/e-lib/internal/domain/entity"
+	"github.com/google/uuid"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+)
+
+func main() {
+	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
+	jsonData, err := os.ReadFile("./testdata/json/books.json")
+	if err != nil {
+		log.Error().Msgf(apperrors.ErrMsgSomethingWentWrong)
+	}
+
+	var books []entity.BookDetail
+	err = json.Unmarshal(jsonData, &books)
+	if err != nil {
+		log.Error().Msgf(apperrors.ErrMsgSomethingWentWrong)
+	}
+
+	for i := range books {
+		newUUID := uuid.New()
+		books[i].UUID = &newUUID
+
+		currentTime := time.Now()
+		books[i].CreatedAt = currentTime
+		books[i].UpdatedAt = currentTime
+	}
+
+	for _, book := range books {
+		fmt.Printf("Title: %s, Copies: %d, UUID: %s, CreatedAt: %s\n", book.Title, book.AvailableCopies, book.UUID, book.CreatedAt)
+	}
 }
